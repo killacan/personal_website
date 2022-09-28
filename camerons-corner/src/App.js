0021dd8c0622f514b9b11065215e963cdf703d20
killacan <kameron000@yahoo.com>
2022-09-28 11:20:30 -0700

    organize components

--- camerons-corner/src/App.js
+++ camerons-corner/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import BottomNav from './components/BottomNav';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <div className="root">
-      
+      <Navbar />
+      <MainPage /> 
+
+      <BottomNav /> 
     </div>
   );
 }
