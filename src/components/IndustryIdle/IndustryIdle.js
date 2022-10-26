e7277bcbaddb7943d70087acdd8bf95dba032f5d
Leisurellama <kameron000@yahoo.com>
2022-10-25 19:41:59 -0500

    css adjustments and fixes

--- src/components/IndustryIdle/IndustryIdle.js
+++ src/components/IndustryIdle/IndustryIdle.js
@@ -1,10 +1,14 @@
-
+import IdleIndustry from "../../assets/Idle-Production-Promo.png"
 
 function IndustryIdle () {
 
     return (
         <div className='industry-idle-container show-box'>
-            <h2>IndustryIdle</h2>
+            <h2 className="text">IndustryIdle</h2>
+
+            <div className="game-img-left">
+                <img className="img" src={IdleIndustry} />
+            </div>
         </div>
     )
 
--- src/components/Turbine/Turbine.js
+++ src/components/Turbine/Turbine.js
@@ -1,4 +1,4 @@
-
+import IdleIndustry from "../../assets/Turbine-Promo.png"
 
 function Turbine () {
 
@@ -7,7 +7,11 @@ function Turbine () {
         <div className='turbine-container show-box'>
             <div className="turbine-text-container">
                 <h2 className="text">Turbine</h2>
-                <p className="text"></p>
+                <p className="text">Turbine is a clone of steam with a working 
+                user auth system, a cart for each user,</p>
+            </div>
+            <div className="game-img-right">
+                <img className="img" src={IdleIndustry} />
             </div>
         </div>
     )
