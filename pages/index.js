f1d6c0943f91ed264d27b159dd5a518d0bc81f25
Leisurellama <kameron000@yahoo.com>
2023-01-17 22:21:35 -0600

    fix black text on Safari

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
       <main className='grid main sm:px-10 md:px-20 lg:px-40 bg-[#020530]'>
         <section className='z-10'>
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl font-burtons'> Cameron's Corner</h1>
+            <h1 className='text-xl font-burtons text-gray-300'> Cameron's Corner</h1>
             <ul className='flex items-center'>
               <li><a href='https://github.com/killacan' target='_blank' className='text-4xl '><AiFillGithub className='hover:scale-125 duration-300'/></a></li>
               <li><a className='bg-gradient-to-r from-blue-800 to-cyan-700 hover:from-cyan-700 hover:to-blue-800 text-white px-4 py-2 rounded-md ml-8' target={'_blank'} href="https://www.linkedin.com/in/cameron-sands-a97183169/">LinkedIn</a></li>
@@ -88,16 +88,16 @@ export default function Home() {
 
         <section>
           <div>
-            <h3 className='text-3xl py-1 w-32 hover:scale-110 duration-300 hover:text-teal-500'> Projects </h3>
+            <h3 className='text-gray-300 text-3xl py-1 w-32 hover:scale-110 duration-300 hover:text-teal-500'> Projects </h3>
             <p className='text-md py-2 leading-8 text-gray-300 pb-4'>
               Here are some of my <span className='text-teal-500'> projects</span>, click on them to check them out.
             </p>
           </div>
           <div className='grid 2xl:grid-cols-3 lg:grid-cols-2 lg:gap-10'>
             <div className='gbox'>
-              <Image src={GameBuddy} width={200} height={200} className='mx-auto' />
-              <h3 className='text-lg font-medium pt-8 pb-2'>Game Buddy</h3>
-              <p className='py-2'>
+              <Image src={GameBuddy} width={350} height={400} className='mx-auto' />
+              <h3 className='text-lg font-medium pt-8 pb-2 text-gray-300'>Game Buddy</h3>
+              <p className='py-2 text-gray-300'>
                 Game Buddy is a web application that allows for people
                 to discover new communities of gamers. Our project features
                 a connection to the Riot API for stat verification in League
@@ -118,9 +118,9 @@ export default function Home() {
               </div>
             </div>
             <div className='gbox'>
-              <Image src={IdleProduction} width={200} height={200} className='mx-auto' />
-              <h3 className='text-lg font-medium pt-8 pb-2'>Idle Production</h3>
-              <p className='py-2'>
+              <Image src={IdleProduction} width={550} height={400} className='mx-auto' />
+              <h3 className='text-gray-300 text-lg font-medium pt-8 pb-2'>Idle Production</h3>
+              <p className='py-2 text-gray-300'>
                 Idle Production is a fun idle game where you can build a massive factory. The game follows a 
                 steady progression of upgraded buildings and increasing amounts of money. The game can be played 
                 for basically ever. 
@@ -138,9 +138,9 @@ export default function Home() {
               </div>
             </div>
             <div className='gbox'>
-              <Image src={TurbinePromo} width={230} height={200} className='mx-auto' />
+              <Image src={TurbinePromo} width={360} height={400} className='mx-auto' />
               <h3 className='text-lg font-medium pt-8 pb-2'>Turbine</h3>
-              <p className='py-2'>
+              <p className='py-2 text-gray-300'>
                 Turbine is a clone of steam with a working 
                 user auth system, a cart for each user, and AWS hosting of all
                 images to reduce server load. 
@@ -165,29 +165,29 @@ export default function Home() {
           <div className='lg:flex lg:flex-row'>
             <div className='py-4'>
               <h2 className='text-3xl py-1 w-40 group hover:scale-110 duration-300 hover:text-teal-500'>About <span className='text-teal-500 group-hover:text-white duration-300'>me</span></h2>
-              <p className='py-2'>I am Cameron Sands and I am a passionate <span className='text-teal-500'> fullstack software developer</span>.</p>
-              <p className='py-2'>As a full stack developer, I have a strong passion for building innovative and dynamic web applications. With a solid foundation in React, Rails, JavaScript, Ruby, and Redux. I am able to create responsive and interactive user interfaces that provide a seamless experience.</p>
-              <p className='py-2'>I have experience working on several projects, including: Game Buddy, an online social platform that facilitates the creation of gaming communities and communication through chat, LlamaChop-Bot, a chatbot that is able to manage Twitch Chat with neural network capabilities, Turbine, a faithful re-representation of Steam utilizing AWS for image hosting and reviews, and Idle Production, an idle game with complex resource management and responsive gameplay. </p>
-              <p className='py-2'>Through my projects I have a proven track record of delivering projects, utilizing my skills in HTML5, CSS3, JavaScript, and Ruby on Rails. I am also a great team player and have experience working in a team environment.</p>
-              <p className='py-2'>I am a graduate of App Academy, where I completed a Curriculum of Study in Web Development (Ruby on Rails, JavaScript, React, and Express) and I hold a Bachelor of Arts in Political Science from San Jose State University. I have a strong passion for game development and participated in the Game Dev club and created a game in Unity with C#.</p>
-              <p className='py-2'>When I'm not working on web development projects, I enjoy pursuing a variety of hobbies. I am an avid Ukulele player and enjoy playing music as a way to relax and unwind. I also enjoy power lifting and have been working on building my strength and fitness.</p>
-              <p className='py-2'>I am a big fan of gaming and have been playing games for as long as I can remember. I also enjoy streaming my gameplay on platforms like Twitch, and making YouTube videos about my gaming experiences. I am always looking for new games to play and new ways to engage with the gaming community.</p>
-              <p className='py-2'>Overall, my hobbies and interests complement my professional skills and help me stay creative and engaged in my work. I am always looking for new ways to improve my skills and grow as a developer, and I believe that my hobbies are an important part of that journey.</p>
-              <p className='py-2'></p>
+              <p className='py-2 text-gray-300'>I am Cameron Sands and I am a passionate <span className='text-teal-500'> fullstack software developer</span>.</p>
+              <p className='py-2 text-gray-300'>As a full stack developer, I have a strong passion for building innovative and dynamic web applications. With a solid foundation in React, Rails, JavaScript, Ruby, and Redux. I am able to create responsive and interactive user interfaces that provide a seamless experience.</p>
+              <p className='py-2 text-gray-300'>I have experience working on several projects, including: Game Buddy, an online social platform that facilitates the creation of gaming communities and communication through chat, LlamaChop-Bot, a chatbot that is able to manage Twitch Chat with neural network capabilities, Turbine, a faithful re-representation of Steam utilizing AWS for image hosting and reviews, and Idle Production, an idle game with complex resource management and responsive gameplay. </p>
+              <p className='py-2 text-gray-300'>Through my projects I have a proven track record of delivering projects, utilizing my skills in HTML5, CSS3, JavaScript, and Ruby on Rails. I am also a great team player and have experience working in a team environment.</p>
+              <p className='py-2 text-gray-300'>I am a graduate of App Academy, where I completed a Curriculum of Study in Web Development (Ruby on Rails, JavaScript, React, and Express) and I hold a Bachelor of Arts in Political Science from San Jose State University. I have a strong passion for game development and participated in the Game Dev club and created a game in Unity with C#.</p>
+              <p className='py-2 text-gray-300'>When I'm not working on web development projects, I enjoy pursuing a variety of hobbies. I am an avid Ukulele player and enjoy playing music as a way to relax and unwind. I also enjoy power lifting and have been working on building my strength and fitness.</p>
+              <p className='py-2 text-gray-300'>I am a big fan of gaming and have been playing games for as long as I can remember. I also enjoy streaming my gameplay on platforms like Twitch, and making YouTube videos about my gaming experiences. I am always looking for new games to play and new ways to engage with the gaming community.</p>
+              <p className='py-2 text-gray-300'>Overall, my hobbies and interests complement my professional skills and help me stay creative and engaged in my work. I am always looking for new ways to improve my skills and grow as a developer, and I believe that my hobbies are an important part of that journey.</p>
+              <p className='py-2 text-gray-300'></p>
             </div>
             <div className='flex flex-col justify-center lg:w-1/2'>
               <h2 className='text-4xl flex justify-center pb-10 pt-4 text-teal-500'>Tech Stack</h2>
               <ul className='grid grid-cols-3 gap-10 justify-center'>
-                <li className='flex justify-center flex-col'><SiRubyonrails className='hover:scale-125 mx-auto duration-300 text-4xl text-red-700' /><p className='flex justify-center'>Ruby on Rails</p></li>
-                <li className='flex justify-center flex-col'><SiJavascript className='hover:scale-125 duration-300 mx-auto text-4xl text-yellow-500' /><p className='flex justify-center'>JavaScript</p></li>
-                <li className='flex justify-center flex-col'><FaReact className='hover:scale-125 duration-300 mx-auto text-4xl text-teal-500' /><p className='flex justify-center'>React</p></li>
-                <li className='flex justify-center flex-col'><SiNextdotjs className='hover:scale-125 duration-300 mx-auto text-4xl' /><p className='flex justify-center'>NextJS</p></li>
-                <li className='flex justify-center flex-col'><SiTailwindcss className='hover:scale-125 duration-300 mx-auto text-4xl text-teal-500' /><p className='flex justify-center'>TailwindCSS</p></li>
-                <li className='flex justify-center flex-col'><AiFillHtml5 className='hover:scale-125 duration-300 mx-auto text-4xl text-orange-600'/><p className='flex justify-center'>HTML5</p></li>
-                <li className='flex justify-center flex-col'><SiCss3 className='hover:scale-125 duration-300 mx-auto text-4xl text-blue-500'/><p className='flex justify-center'>CSS3</p></li>
-                <li className='flex justify-center flex-col'><SiNodedotjs className='hover:scale-125 duration-300 mx-auto text-4xl text-green-700'/><p className='flex justify-center'>NodeJS</p></li>
-                <li className='flex justify-center flex-col'><SiRedux className='hover:scale-125 duration-300 mx-auto text-4xl text-indigo-700'/><p className='flex justify-center'>Redux</p></li>
-                <li className='flex justify-center flex-col'><SiMongodb className='hover:scale-125 duration-300 mx-auto text-4xl text-green-600'/><p className='flex justify-center'>MongoDB</p></li>
+                <li className='flex justify-center flex-col'><SiRubyonrails className='hover:scale-125 mx-auto duration-300 text-4xl text-red-700' /><p className='flex  justify-center text-gray-300'>Ruby on Rails</p></li>
+                <li className='flex justify-center flex-col'><SiJavascript className='hover:scale-125 duration-300 mx-auto text-4xl text-yellow-500' /><p className='flex justify-center text-gray-300'>JavaScript</p></li>
+                <li className='flex justify-center flex-col'><FaReact className='hover:scale-125 duration-300 mx-auto text-4xl text-teal-500' /><p className='flex justify-center text-gray-300'>React</p></li>
+                <li className='flex justify-center flex-col'><SiNextdotjs className='hover:scale-125 duration-300 mx-auto text-4xl' /><p className='flex justify-center text-gray-300'>NextJS</p></li>
+                <li className='flex justify-center flex-col'><SiTailwindcss className='hover:scale-125 duration-300 mx-auto text-4xl text-teal-500' /><p className='flex justify-center text-gray-300'>TailwindCSS</p></li>
+                <li className='flex justify-center flex-col'><AiFillHtml5 className='hover:scale-125 duration-300 mx-auto text-4xl text-orange-600'/><p className='flex justify-center text-gray-300'>HTML5</p></li>
+                <li className='flex justify-center flex-col'><SiCss3 className='hover:scale-125 duration-300 mx-auto text-4xl text-blue-500'/><p className='flex justify-center text-gray-300'>CSS3</p></li>
+                <li className='flex justify-center flex-col'><SiNodedotjs className='hover:scale-125 duration-300 mx-auto text-4xl text-green-700'/><p className='flex justify-center text-gray-300'>NodeJS</p></li>
+                <li className='flex justify-center flex-col'><SiRedux className='hover:scale-125 duration-300 mx-auto text-4xl text-indigo-700'/><p className='flex justify-center text-gray-300'>Redux</p></li>
+                <li className='flex justify-center flex-col'><SiMongodb className='hover:scale-125 duration-300 mx-auto text-4xl text-green-600'/><p className='flex justify-center text-gray-300'>MongoDB</p></li>
               </ul>
             </div>
           </div>
@@ -276,9 +276,9 @@ export default function Home() {
             }}
         />
       </main>
-      <section className='sm:px-10 md:px-20 lg:px-40'>
+      <section className='sm:px-10 md:px-20 lg:px-40 bg-gray-900'>
         <nav className='py-10 flex justify-between'>
-          <h1 className='text-3xl'> Find me on <a className='text-blue-500' href='https://www.linkedin.com/in/cameron-sands-a97183169/' target='_blank'>LinkedIn</a> or <a className='text-blue-500' href='https://github.com/killacan' target='_blank'>Github</a></h1>
+          <h1 className='text-3xl text-gray-300'> Find me on <a className='text-blue-500' href='https://www.linkedin.com/in/cameron-sands-a97183169/' target='_blank'>LinkedIn</a> or <a className='text-blue-500' href='https://github.com/killacan' target='_blank'>Github</a></h1>
           <ul className='flex items-center'>
             <li><a href='https://github.com/killacan' target='_blank' className='text-4xl'><AiFillGithub className='hover:scale-125 duration-300'/></a></li>
             <li><a className='bg-gradient-to-r from-blue-800 to-cyan-700 hover:from-cyan-700 hover:to-blue-800 text-white px-4 py-2 rounded-md ml-8' target={'_blank'} href="https://www.linkedin.com/in/cameron-sands-a97183169/">LinkedIn</a></li>
